967dffdbe2d8a036a6fa4b2afa10f02bd53e41a0
ThomaasB <tbdesign@outlook.com>
2023-03-10 13:54:13 +0100

    All in 1

--- main.js
+++
@@ -1,6 +0,0 @@
-function toggleBaresthoReservation() {
-    let baresthowidget = document.querySelector("#barestho-widget")
-    baresthowidget.classList.toggle("barestho-closed")
-    let baresthobutton = document.querySelector("#barestho-button")
-    baresthobutton.innerHTML = baresthowidget.classList.contains("barestho-closed") ? "Réserver" : "Fermer"
-}
---
+++ script.js
@@ -0,0 +1,6 @@
+function onToggleOpen() {
+    const widget = document.querySelector("#barestho-widget-container");
+    const button = document.querySelector("#barestho-button");
+    widget.classList.toggle('open');
+    button.innerHTML = widget.classList.contains('open')? "Fermer": "Réserver";
+}
