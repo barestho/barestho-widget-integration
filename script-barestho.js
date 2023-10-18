30cf4fe785463c203ec7e4120280dc066492cc42
ThomaasB <tbdesign@outlook.com>
2023-10-18 14:50:30 +0200

    button int

--- script-barestho.js
+++ script-barestho.js
@@ -4,3 +4,9 @@ function onBaresthoToggle() {
     widget.classList.toggle('open');
     button.innerHTML = widget.classList.contains('open')? "Fermer": "Réserver";
 }
+
+
+function BaresthoOpenModal() {
+    const modalWidgetContainer = document.querySelector("#barestho-widget-container.modal");
+    modalWidgetContainer.classList.contains('open')?modalWidgetContainer.classList.remove('open'):modalWidgetContainer.classList.add('open')
+}
