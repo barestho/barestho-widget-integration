cc9bf9874b5dfb1842ed5e18b4ea6796d1c2f619
ThomaasB <tbdesign@outlook.com>
2023-10-18 15:05:56 +0200

    All-in-one

--- script-barestho.js
+++ script-barestho.js
@@ -1,12 +1,16 @@
-function onBaresthoToggle() {
-    const widget = document.querySelector("#barestho-widget-container");
+let widgetContainer = document.querySelector("#barestho-widget-container");
+let isModal = false;
+
+function toggleMode(mode) {
+    if (mode === 'default') {
+        widgetContainer.classList.remove('modal');
+        widgetContainer.classList.add('default');
+    } else if (mode === 'modal') {
+        widgetContainer.classList.remove('default');
+        widgetContainer.classList.add('modal');
+    }
+    isModal = !isModal; // Inverse l'état
+    widgetContainer.classList.toggle('open', isModal);
     const button = document.querySelector("#barestho-button");
-    widget.classList.toggle('open');
-    button.innerHTML = widget.classList.contains('open')? "Fermer": "Réserver";
-}
-
-
-function BaresthoOpenModal() {
-    const modalWidgetContainer = document.querySelector("#barestho-widget-container.modal");
-    modalWidgetContainer.classList.contains('open')?modalWidgetContainer.classList.remove('open'):modalWidgetContainer.classList.add('open')
+    button.innerHTML = isModal ? "Fermer" : "Réserver";
 }
