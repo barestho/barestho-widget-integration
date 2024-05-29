e30fd8b6add9cfecf7a5e8ae689c81c22265ad76
ThomaasB <tbdesign@outlook.com>
2024-05-29 11:22:40 +0200

    Refactor : complete overhaul of the css code to make it cleaner

--- barestho-widget-script.js
+++ barestho-widget-script.js
@@ -5,3 +5,18 @@ function onToggle() {
     button.innerHTML = widget.classList.contains('open')? "Fermer": "Réserver";
 }
 
+document.addEventListener('DOMContentLoaded', function() {
+    const iframe = document.getElementById('barestho-widget');
+    const iframeSrc = iframe.src;
+    const url = new URL(iframeSrc);
+    const iframeOrigin = url.origin;
+
+    window.addEventListener('message', function(event) {
+        if (event.origin === iframeOrigin) {
+            const data = event.data;
+            if (data.height) {
+                iframe.style.height = data.height + 'px';
+            }
+        }
+    });
+});
