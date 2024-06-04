63d12a82bc2a954ef33405580fe79973e5e1c2dd
ThomaasB <tbdesign@outlook.com>
2024-06-04 16:02:20 +0200

    chor : trying popupview

--- barestho-widget-script.js
+++ barestho-widget-script.js
@@ -13,3 +13,19 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    const iframe = document.getElementById('barestho-widget-popup');
+    const iframeSrc = iframe.src;
+    const url = new URL(iframeSrc);
+    const iframeOrigin = url.origin;
+
+    window.addEventListener('message', function(event) {
+        if (event.origin === iframeOrigin) {
+            const data = event.data;
+            if (data.height) {
+                iframe.style.height = data.height + 'px';
+            }
+        }
+    });
+});
