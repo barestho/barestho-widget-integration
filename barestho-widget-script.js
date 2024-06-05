af3c83562abb8472971c88862cdebc3a1f43325d
ThomaasB <tbdesign@outlook.com>
2024-06-05 12:23:49 +0200

    chor : nettoyage

--- barestho-widget-script.js
+++ barestho-widget-script.js
@@ -14,24 +14,6 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-    const iframe = document.getElementById('barestho-widget-popup');
-    const iframeSrc = iframe.src;
-    const url = new URL(iframeSrc);
-    const iframeOrigin = url.origin;
-
-    window.addEventListener('message', function(event) {
-        if (event.origin === iframeOrigin) {
-            const data = event.data;
-            if (data.height) {
-                iframe.style.height = data.height + 'px';
-            }
-        }
-    });
-});
-
-
-
 function BaresthoToggleIframe() {
     const iframeContainer = document.getElementById('barestho-widget-popup-container');
     if (iframeContainer.style.display === 'none' || !iframeContainer.style.display) {
