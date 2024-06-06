cb437cd3264f1acc2cfc9a4a6867d7e9521dc6f0
ThomaasB <tbdesign@outlook.com>
2024-06-06 09:48:20 +0200

    chor : Reception and detection if we click on the reserve button in the react + iframe project

--- barestho-widget-script.js
+++ barestho-widget-script.js
@@ -1,18 +1,60 @@
+/* Reception and detection if we click on the reserve button in the react + iframe project */
+
+let buttonClicked = false;
+
+function updateStyles() {
+  const widgetToggle = document.getElementById('barestho-widget-toggle');
+  
+  if (window.innerWidth < 450) {
+    if (buttonClicked) {
+      widgetToggle.style.minHeight = '100vh';
+      console.log("oui");
+    } else {
+      widgetToggle.style.minHeight = '56px';
+      console.log("non");
+    }
+  }
+}
+
+window.addEventListener('message', function(event) {
+  const iframe = document.getElementById('barestho-widget-toggle');
+  if (event.source === iframe.contentWindow) {
+    const newButtonClicked = event.data.buttonClicked;
+    
+    if (newButtonClicked !== undefined && newButtonClicked !== buttonClicked) {
+      buttonClicked = newButtonClicked;
+      updateStyles();
+    }
+  }
+});
+
 document.addEventListener('DOMContentLoaded', function() {
+  updateStyles();
+});
+
+
+
+/* Receiving and detecting iframe size and content */
+
+document.addEventListener('DOMContentLoaded', function() {
+  if (window.innerWidth >= 481) {
     const iframe = document.getElementById('barestho-widget-toggle');
     const iframeSrc = iframe.src;
     const url = new URL(iframeSrc);
     const iframeOrigin = url.origin;
 
     window.addEventListener('message', function(event) {
-        if (event.origin === iframeOrigin) {
-            const data = event.data;
-            if (data.height) {
-                iframe.style.height = data.height + 'px';
-            }
+      if (event.origin === iframeOrigin) {
+        const data = event.data;
+        if (data.height) {
+          iframe.style.height = data.height + 'px';
         }
+      }
     });
+  }
 });
+
+/* Manage the iframe display alone */
 
 function BaresthoToggleIframe() {
     const iframeContainer = document.getElementById('barestho-widget-popup-container');
