bb869be9515673ea02c911865bcca0c86a2fbb7c
ThomaasB <tbdesign@outlook.com>
2024-07-04 17:25:12 +0200

    chore: fix button popup for noscroll body

--- script-barestho.js
+++ script-barestho.js
@@ -1,5 +1,3 @@
-/* Reception and detection if we click on the reserve button in the react + iframe project */
-
 let isToggleOpen = false;
 
 function updateToggleStyles() {
@@ -29,7 +27,6 @@ document.addEventListener('DOMContentLoaded', function() {
   updateToggleStyles();
 });
 
-/* Receiving and detecting iframe size and content */
 
 document.addEventListener('DOMContentLoaded', function() {
   if (window.innerWidth >= 481) {
@@ -49,23 +46,24 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
-/* Manage the toggle button function */
+
+let isPopupOpen = {};
 
 function baresthoToggleIframe(id) {
-  // Si aucun id n'est spécifié, id vaut '1' par défaut
   if (!id) {
       id = '1';
   }
 
-  // Vérifie s'il existe un conteneur avec l'id complet ou sans le suffixe '-1'
   const iframeContainer = document.getElementById(`barestho-widget-popup-container-${id}`)
-      || document.getElementById(`barestho-widget-popup-container`);
-  
+      || document.getElementById('barestho-widget-popup-container');
+
   if (iframeContainer) {
       if (iframeContainer.style.display === 'none' || !iframeContainer.style.display) {
           iframeContainer.style.display = 'block';
+          document.body.style.overflow = 'hidden'; 
       } else {
           iframeContainer.style.display = 'none';
+          document.body.style.overflow = ''; 
       }
   } else {
       console.error(`Container with id barestho-widget-popup-container-${id} not found.`);
@@ -73,26 +71,22 @@ function baresthoToggleIframe(id) {
 }
 
 
-/* Réception du bouton de la croix pour fermer le widget en mode popup */
-
-let isPopupOpen = {};
 
 function updatePopUpStyles(id) {
-    // Si id n'est pas défini ou est une chaîne vide, id vaut '1' par défaut
     if (!id) {
         id = '1';
     }
-
-    // Vérifie s'il existe un conteneur avec l'id complet ou sans le suffixe '-1'
     const popupContainer = document.getElementById(`barestho-widget-popup-container-${id}`)
         || document.getElementById('barestho-widget-popup-container');
 
     if (popupContainer) {
         if (isPopupOpen[id] === false) {
             popupContainer.style.display = 'block';
+            document.body.style.overflow = 'hidden'; 
             isPopupOpen[id] = true;
         } else {
             popupContainer.style.display = 'none';
+            document.body.style.overflow = ''; 
             isPopupOpen[id] = false;
         }
     } else {
