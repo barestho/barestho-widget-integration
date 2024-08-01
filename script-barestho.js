cd52da890799bfd340974adc1e663b1c9590ebe1
Nicolas Jeuniaux <nicolas.jeuniaux@barestho.com>
2024-08-01 14:52:50 +0200

    chore: added barestho widget manager

--- script-barestho.js
+++ script-barestho.js
@@ -12,6 +12,8 @@
 
 const BARESTHO_WIDGET_ID = "barestho-widget";
 
+const BARESTHO_BACKGROUND_POPUP_ID = "barestho-popup-background";
+
 const BARESTHO_WIDGET_VIEW_MODES = {
   STANDALONE: "standalone",
   INPAGE: "in-page",
@@ -19,94 +21,87 @@ const BARESTHO_WIDGET_VIEW_MODES = {
   POPUP: "popup",
 }
 
+class BaresthoWidgetManager {
 
-// baresthoMain
-
-function baresthoManageBackgroundPopUp(action) {
-  const backgroundDiv = document.getElementById('background-overlay-barestho');
-  if (action === 'create' && !backgroundDiv) {
-    const newBackgroundDiv = document.createElement('div');
-    newBackgroundDiv.id = 'background-overlay-barestho';
-    document.body.appendChild(newBackgroundDiv);
-    document.body.style.overflow = 'hidden'; 
-  } else if (action === 'remove' && backgroundDiv) {
-    backgroundDiv.remove();
-    document.body.style.overflow = ''; 
+  /**
+   * @param {"create" | "remove"} action 
+   */
+  managePopupBackground(action) {
+    const backgroundDiv = document.getElementById(BARESTHO_BACKGROUND_POPUP_ID);
+    if (action === 'create' && !backgroundDiv) {
+      const newBackgroundDiv = document.createElement('div');
+      newBackgroundDiv.id = BARESTHO_BACKGROUND_POPUP_ID;
+      document.body.appendChild(newBackgroundDiv);
+      document.body.style.overflow = 'hidden'; 
+    } else if (action === 'remove' && backgroundDiv) {
+      backgroundDiv.remove();
+      document.body.style.overflow = ''; 
+    }
   }
-}
-function baresthoOpenPopup(id) {
-  const widget = document.querySelector(`.${BARESTHO_WIDGET_ID}.${BARESTHO_WIDGET_VIEW_MODES.POPUP}#${id}`);
-  widget?.classList.add("open");
-  baresthoManageBackgroundPopUp('create');
-}
 
-/**
- * Handles iframe messsages.
- * @param {MessageEvent<{ type: string, payload: any }>} e 
- * @param {HTMLIFrameElement} widget 
- */
-function baresthoHandleMessage(e, widget) {  
-  const { type, payload } = e.data;
+  openPopup() {
+    const widget = document.querySelector(`.${BARESTHO_WIDGET_ID}.${BARESTHO_WIDGET_VIEW_MODES.POPUP}#${id}`);
+    widget?.classList.add("open");
+    this.managePopupBackground('create');
+  }
+
+  /**
+   * Handles iframe messsages.
+   * @param {MessageEvent<{ type: string, payload: any }>} e 
+   * @param {HTMLIFrameElement} widget 
+   */
+  handleMessage(e, widget) {
+    const { type, payload } = e.data;
   
-  switch(type) {
-    case "popup": {
-      widget.classList.remove("open");
-      baresthoManageBackgroundPopUp('remove'); 
-      break;
+    switch(type) {
+      case "popup": {
+        widget.classList.remove("open");
+        this.managePopupBackground('remove'); 
+        break;
+      }
+      case "toggle": {
+        const isOpen = payload.state ?? false;
+        if (isOpen && window.innerWidth < 450) {
+          widget.classList.add("open")
+        } else widget.classList.remove("open");
+        break;
+      }
+      case "resize": {
+        widget.style.height = `${payload.height}px`;
+        break;
+      }
+      default:
+        console.warn(`Unknown message type "${type}".`);
+        break;
     }
-    case "toggle": {
-      const isOpen = payload.state ?? false;
-      if (isOpen && window.innerWidth < 450) {
-        widget.classList.add("open")
-      } else widget.classList.remove("open");
-      break;
-    }
-    case "resize": {
-      widget.style.height = `${payload.height}px`;
-      break;
-    }
-    default:
-      console.warn(`Unknown message type "${type}".`);
-      break;
   }
 
-}
+  main() {
+    const widgets = document.querySelectorAll(`.${BARESTHO_WIDGET_ID}`);
 
+    for (const widget of widgets) {
+      const src = widget.getAttribute("src");
 
-/*
-  Modes:
-    - standalone (default): just the reservation app without any mode applied.
-    - toggle: a short version of the form with a button that hides and displays it.
-    - popup: a short version of the form with logic to display a modal like view. It requires more config.
-    - inpage: a short version of the form.
-*/
+      const srcUrl = new URL(src);
 
+      const widgetMode = srcUrl.searchParams.get("view") ?? BARESTHO_WIDGET_VIEW_MODES.STANDALONE;
 
-function baresthoMain() {
+      widget.classList.add(widgetMode);
+    }
 
-  const widgets = document.querySelectorAll(`.${BARESTHO_WIDGET_ID}`);
+    // Message dispatcher
+    window.addEventListener("message", e => {
 
-  for (const widget of widgets) {
-    const src = widget.getAttribute("src");
+      const foundWidget = Array.from(widgets)
+        .find(widget => widget.contentWindow === e.source);
 
-    const srcUrl = new URL(src);
-
-    const widgetMode = srcUrl.searchParams.get("view") ?? BARESTHO_WIDGET_VIEW_MODES.STANDALONE;
-
-    widget.classList.add(widgetMode);
+      if(foundWidget)
+        this.handleMessage(e, foundWidget);
+    });
   }
-
-  // Message dispatcher
-  window.addEventListener("message", e => {
-
-    const foundWidget = Array.from(widgets)
-      .find(widget => widget.contentWindow === e.source);
-
-    if(foundWidget)
-      baresthoHandleMessage(e, foundWidget);
-  });
-
 }
+
+window.BARESTHO = new BaresthoWidgetManager();
 
 // baresthoMain is executed if DOM is fully loaded.
-document.addEventListener('DOMContentLoaded', baresthoMain);
+document.addEventListener('DOMContentLoaded', window.BARESTHO.main);
